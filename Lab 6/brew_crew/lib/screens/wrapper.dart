299754c6fbde826aa0453759c7809798ef8769d4
aidilaqif <aidil.aqif@gmail.com>
2023-06-19 00:44:41 +0800

    main

---
+++ Lab 6/brew_crew/lib/screens/wrapper.dart
@@ -0,0 +1,22 @@
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../model/person.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //accessing user data from provider
+    final user = Provider.of<Person?>(context);
+
+    //return either home or authenticate
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+  }
+}
---
+++ Lab 6/brew_crew/lib/services/auth.dart
@@ -0,0 +1,68 @@
+//for authentication service
+import 'package:brew_crew/model/person.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based on userCredential
+  Person? _userFromFirebaseUser(User? user) {
+    return user != null ? Person(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<Person?> get user {
+    return _auth
+        .authStateChanges()
+        // .map((User? user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
+  //sign in anonymously
+  Future signInAnon() async {
+    try {
+      final UserCredential result =
+          await FirebaseAuth.instance.signInAnonymously();
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+  //sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      final UserCredential result = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      final UserCredential result = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
